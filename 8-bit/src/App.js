e41732c3d2005bff0e6ef684733d939a0e950e95
Goh Yong Jie <gohyongjie1112@gmail.com>
2021-05-25 20:36:33 +0800

    Resolve conflict

--- 8-bit/src/App.js
+++ 8-bit/src/App.js
@@ -1,56 +1,15 @@
-<<<<<<< HEAD
-import Header from "./components/Header"
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import Breakout from "./pages/Breakout"
-import "./App.css"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-=======
 import Header from './components/Header';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 import SignUp from './components/SignUp/SignUp';
 import Login from './components/Login/Login';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { AuthProvider } from './components/Context/AuthContext';
->>>>>>> Login
+import Breakout from "./pages/Breakout";
 
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
-      <div className="App">
-        <div className="Header"></div>
-<<<<<<< HEAD
-          <Header />
-          <div className="Content">
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
-              <Route path="/LoginPage">
-                <LoginPage />
-              </Route>
-            </Switch>
-          </div>
-=======
-        <Header />
-        <div className="Content">
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route path="/LoginPage">
-              <LoginPage />
-            </Route>
-            <Route path="/Breakout">
-              <Breakout />
-            </Route>
-          </Switch>
-        </div>
->>>>>>> Breakout
-      </div>
-=======
       <AuthProvider>
           <div className="App">
             <div className="Header"></div>
@@ -66,11 +25,13 @@ function App() {
                   <Route path="/Login">
                     <Login />
                   </Route> 
+                  <Route path="/Breakout">
+                    <Breakout />
+                  </Route>
                 </Switch>
               </div>
           </div>
       </AuthProvider>
->>>>>>> Login
     </Router>
   )
 }
