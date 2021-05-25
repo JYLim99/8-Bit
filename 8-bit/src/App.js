68b2ede3e24579ce30b504bbb65d0b759e7d5d11
Goh Yong Jie <gohyongjie1112@gmail.com>
2021-05-25 20:05:51 +0800

    Add login and sign up

--- 8-bit/src/App.js
+++ 8-bit/src/App.js
@@ -1,26 +1,33 @@
-import Header from "./components/Header";
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import "./App.css";
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import SignUp from './components/SignUp/SignUp';
+import Login from './components/Login/Login';
+import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AuthProvider } from './components/Context/AuthContext';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <div className="Header"></div>
-          <Header />
-          <div className="Content">
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
-              <Route path="/LoginPage">
-                <LoginPage />
-              </Route>
-            </Switch>
+      <AuthProvider>
+          <div className="App">
+            <div className="Header"></div>
+              <Header />
+              <div className="Content">
+                <Switch>
+                  <Route exact path="/">
+                    <HomePage />
+                  </Route>
+                  <Route exact path="/SignUp">
+                    <SignUp />
+                  </Route>
+                  <Route path="/Login">
+                    <Login />
+                  </Route> 
+                </Switch>
+              </div>
           </div>
-      </div>
+      </AuthProvider>
     </Router>
   );
 }
