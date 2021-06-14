e3285f6436354b9f7db82ca064f2f332d966b918
Goh Yong Jie <gohyongjie1112@gmail.com>
2021-06-14 17:41:04 +0800

    Change menu

--- 8-bit/src/App.js
+++ 8-bit/src/App.js
@@ -1,46 +1,53 @@
-import Header from "./components/Header"
-import HomePage from "./pages/HomePage/HomePage"
-import SignUp from "./components/SignUp/SignUp"
-import Login from "./components/Login/Login"
-import "./App.css"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { AuthProvider } from "./components/Context/AuthContext"
-import Breakout from "./pages/Breakout"
-import Pong from "./components/Pong/Pong"
-import SpaceInvaders from "./components/GameSpaceInvaders/Game/Game"
-import ProfilePage from "./pages/ProfilePage"
+import Header from "./components/Header";
+import { useState, Fragment } from "react";
+import Menu from "./components/Menu";
+import HomePage from "./pages/HomePage/HomePage";
+import SignUp from "./components/SignUp/SignUp";
+import Login from "./components/Login/Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "./components/Context/AuthContext";
+import './App.css';
+import Breakout from "./pages/Breakout";
+import Pong from "./components/Pong/Pong";
+import SpaceInvaders from "./components/GameSpaceInvaders/Game/Game";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <div className="App">
-          <div className="Header">
-            <Header />
-          </div>
           <div className="Content">
             <Switch>
-              <Route exact path="/">
-                <HomePage />
+              <Route exact path="/Menu">
+                  <Menu />
               </Route>
-              <Route exact path="/SignUp">
-                <SignUp />
-              </Route>
-              <Route path="/Login">
-                <Login />
-              </Route>
-              <Route path="/ProfilePage">
-                <ProfilePage />
-              </Route>
-              <Route path="/SpaceInvaders">
-                <SpaceInvaders />
-              </Route>
-              <Route path="/Breakout">
-                <Breakout />
-              </Route>
-              <Route path="/Pong">
-                <Pong />
-              </Route>
+              <Fragment>
+                <div className="Header">
+                <Header />
+                </div>
+                <Route exact path="/">
+                  <HomePage />
+                </Route>
+                <Route exact path="/SignUp">
+                  <SignUp />
+                </Route>
+                <Route path="/Login">
+                  <Login />
+                </Route>
+                <Route path="/ProfilePage">
+                  <ProfilePage />
+                </Route>
+                <Route path="/SpaceInvaders">
+                  <SpaceInvaders />
+                </Route>
+                <Route path="/Breakout">
+                  <Breakout />
+                </Route>
+                <Route path="/Pong">
+                  <Pong />
+                </Route>
+              </Fragment>
             </Switch>
           </div>
         </div>
--- 8-bit/src/components/Header/Header.js
+++ 8-bit/src/components/Header/Header.js
@@ -1,44 +1,24 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SpaceInvaderIcon from '../../images/SpaceInvaderIcon.png';
 import styles from './Header.module.css';
-import { useAuth } from '../Context/AuthContext';
 
 const Header = () => {
-
-    const { currentUser, logout } = useAuth();
-    const history = useHistory();
-
-    function handleLogout() {
-        logout();
-        history.push('/Login');
-    }
-    
-    if (!currentUser) {
-        return ( 
-            <div className={ styles.Header }>
-                <img className={ styles.SpaceInvaderIcon } src={ SpaceInvaderIcon } alt="SpaceInvaderIcon"/>
-                <Link to="/" className={ styles.Name }> 8 Bit </Link>
-                <nav className={ styles.LinkContainer }>
-                    <Link to="/" className={ styles.HomeLink }> Home </Link>
-                    <Link to="/Forum" className={ styles.ForumLink }> Forum </Link>
-                    <Link to="/Login" className={ styles.LoginLink }> Login </Link>
-                </nav>
+    return ( 
+        <div className={ styles.Header }>
+            <div className={ styles.container }>
+                <div className={ styles.HeaderInner }>
+                    <img className={ styles.SpaceInvaderIcon } src={ SpaceInvaderIcon } alt="SpaceInvaderIcon"/>
+                    <Link to="/" className={ styles.Name }> 8 Bit </Link>
+                    <Link to="/Menu">
+                        <div className={ styles.nav }>
+                            <span className={ styles.span }></span>
+                            <span className={ styles.span }></span>
+                        </div>
+                    </Link>
+                </div>
             </div>
-        );
-    } else {
-        return ( 
-            <div className={ styles.Header }>
-                <img className={ styles.SpaceInvaderIcon } src={ SpaceInvaderIcon } alt="SpaceInvaderIcon"/>
-                <Link to="/" className={ styles.Name }> 8 Bit </Link>
-                <nav className={ styles.LinkContainer }>
-                    <Link to="/" className={ styles.HomeLink }> Home </Link>
-                    <Link to="/Forum" className={ styles.ForumLink }> Forum </Link>
-                    <Link to="/ProfilePage" className={ styles.ProfileLink }> Profile </Link>
-                    <button className={ styles.Button } onClick= { handleLogout }> Logout </button>
-                </nav>
-            </div>
-        );
-    }
+         </div>
+    );
 }
 
 export default Header;
