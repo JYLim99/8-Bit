67c88dacd730d9f14196f85a56885f4503ca87fb
Goh Yong Jie <gohyongjie1112@gmail.com>
2021-06-25 20:37:10 +0800

    Edit Home page

--- 8-bit/src/pages/HomePage/HomePage.js
+++ 8-bit/src/pages/HomePage/HomePage.js
@@ -3,7 +3,10 @@ import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div></div>
+    <div className={ styles.background }>
+      <div className={ styles.header }> 8 Bit </div>
+      <button className={ styles.getStartedBtn }> Get started </button>
+    </div>
   );
 };
 
